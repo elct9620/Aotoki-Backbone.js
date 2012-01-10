72e5b4f9a2d47b55c101621cea1b75d2ed53da4a
Aotoki <elct9620@frost.tw>
2012-01-10 12:55:23 +0800

    init orm and add note model

--- index.php
+++ index.php
@@ -20,6 +20,12 @@ $app = new Slim(
 		)
 	);
 
+/* Initialize ORM */
+ORM::configure('mysql:host=localhost;dbname=backbone.js');
+ORM::configure('username', 'root');
+ORM::configure('password', 'root');
+ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
 /* Load Models */
 
 /* Load APP */
